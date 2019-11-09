07416086e2bbb6e14585561a2c841c5194d8a8e9
aaii <a.1999.ansari@gmail.com>
2019-11-09 21:30:28 +0330

    That's a the simplest script in Bash

---
+++ alarm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read -p "Tell me the time you want wake up:(Example 13:23) " TM
+echo "$TM"
+TIME=$(date "+%T"| cut -d ":" -f 1,2 )
+echo "$TIME"
+while [ "$TIME" != "$TM" ]
+do	
+	echo "Hanuz vaqthesh nashode"
+	TIME=$(date "+%T"| cut -d ":" -f 1,2 )
+	echo "$TIME" 
+	echo "$TM"
+	sleep 5
+	clear
+done
+echo "Wake up!!"
+espeak "Wake up baby!"
+espeak "It's time to work"
